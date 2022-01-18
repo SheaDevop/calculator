5a7641a9b33fd177acf882caf4e2066d9f21b58b
SheaDevop <nacho.dieguez18@gmail.com>
2022-01-17 21:59:17 -0300

    js waiting 4 debugging

--- index.js
+++ index.js
@@ -22,12 +22,90 @@ function operate (operator, a, b) {
 function populate () {
     let previousContent = calculatorDisplay.textContent
     calculatorDisplay.textContent = previousContent + this.textContent
+    displayValue = Number(calculatorDisplay.textContent)
 }
 
 let calculatorDisplay = document.getElementById("display")
 
-let numbuttons = document.querySelectorAll('.numBtn')
+let displayValue 
+let firstValue
+let equalsValue
+let selectedOperator
 
-numbuttons.forEach((btn) => {
+let numButtons = document.querySelectorAll('.numBtn')
+
+numButtons.forEach((btn) => {
     btn.addEventListener('click', populate)
+})
+
+let addBtn = document.getElementById("addBtn")
+addBtn.addEventListener('click', () => {
+    if(selectedOperator === undefined) {
+        firstValue = displayValue
+        calculatorDisplay.textContent = ''
+        selectedOperator = add
+    }else{
+        calculatorDisplay.textContent = operate(selectedOperator, firstValue, displayValue)
+        firstValue = Number(calculatorDisplay.textContent)
+        selectedOperator = add
+    }
+})
+
+let subtractBtn = document.getElementById("subtractBtn")
+subtractBtn.addEventListener('click', () => {
+    if(selectedOperator === undefined) {
+        firstValue = displayValue
+        calculatorDisplay.textContent = ''
+        selectedOperator = subtract
+    }else{
+        calculatorDisplay.textContent = operate(selectedOperator, firstValue, displayValue)
+        firstValue = Number(calculatorDisplay.textContent)
+        selectedOperator = subtract
+    }
+})
+
+let multiplyBtn = document.getElementById("multiplyBtn")
+multiplyBtn.addEventListener('click', () => {
+    if(selectedOperator === undefined) {
+        firstValue = displayValue
+        calculatorDisplay.textContent = ''
+        selectedOperator = multiply
+    }else{
+        calculatorDisplay.textContent = operate(selectedOperator, firstValue, displayValue)
+        firstValue = Number(calculatorDisplay.textContent)
+        selectedOperator = multiply
+    }
+})
+
+let divideBtn = document.getElementById("divideBtn")
+divideBtn.addEventListener('click', () => {
+    if(selectedOperator === undefined) {
+        firstValue = displayValue
+        calculatorDisplay.textContent = ''
+        selectedOperator = divide
+    }else{
+        calculatorDisplay.textContent = operate(selectedOperator, firstValue, displayValue)
+        firstValue = Number(calculatorDisplay.textContent)
+        selectedOperator = divide
+    }
+})
+
+let equalsBtn = document.getElementById("equalsBtn")
+equalsBtn.addEventListener('click', () => {
+    if(equalsValue === undefined) {
+        calculatorDisplay.textContent = operate(selectedOperator, firstValue, displayValue)
+        equalsValue = Number(calculatorDisplay.textContent)
+    }else {
+        calculatorDisplay.textContent = operate(selectedOperator, equalsValue, displayValue)
+        equalsValue = Number(calculatorDisplay.textContent)
+    }
+})
+
+let clearBtn = document.getElementById("clearBtn")
+clearBtn.addEventListener('click', () => {
+    firstValue = undefined
+    calculatorDisplay.textContent = ''
+    selectedOperator = undefined
+    equalsValue = undefined
+    displayValue = undefined
 })
